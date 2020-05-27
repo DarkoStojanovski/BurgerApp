2c9aff89f426d3cc9b12c57b1bbfb9e48241b857
Darko Stojanovski <darkoluis@gmail.com>
2020-05-27 11:35:24 -0400

    Burger app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, devoured) VALUES ('BigMac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Quater Pound with Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whoper', true);
+INSERT INTO burgers (name) VALUES ('Bacon Cheesburger');
